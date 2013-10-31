a5abfc0ee8ab0c77c1b5c7ca080e214877757865
Esben Sorig <esben.sorig.12@ucl.ac.uk>
2013-10-31 12:37:18 +0000

    Reverted

--- registration/search.php
+++ registration/search.php
@@ -42,28 +42,24 @@
     $search_term = $_POST['query'];
 
     if($search_term != '') {
-    $search_term_esc = AddSlashes($search_term);
 
-    $sql_select = "SELECT * FROM registration_tbl WHERE name LIKE '%$search_term_esc%' OR email LIKE '%$search_term_esc%' OR company_name LIKE '%$search_term_esc%'";
-    $stmt = $conn->query($sql_select);
-    $registrants = $stmt->fetchAll(); 
-    if(count($registrants) > 0) {
+    $stmt = $pdo->prepare("SELECT * FROM registration_tbl WHERE name LIKE :query OR email LIKE :query OR company_name LIKE :query");
+    $stmt = $stmt->execute(array('query' => $search_term));
+    
         echo "<h2>Search results:</h2>";
         echo "<table>";
         echo "<tr><th>Name</th>";
         echo "<th>Email</th>";
         echo "<th>Date</th>";
 	echo "<th>Company name</th></tr>";
-        foreach($registrants as $registrant) {
+        foreach($stmt as $registrant) {
             echo "<tr><td>".$registrant['name']."</td>";
             echo "<td>".$registrant['email']."</td>";
             echo "<td>".$registrant['date']."</td>";
 	    echo "<td>".$registrant['company_name']."</td></tr>";
         }
         echo "</table>";
-    } else {
-        echo "<h3>No results.</h3>";
-    }
+    
     }
 ?>
 </body>
